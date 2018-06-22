65e775dc370f12a363400d3835a73129f788b379
Wroud <wrouds@gmail.com>
2018-06-23 00:05:42 +0300

    1.0.8 update build

--- lib/interfaces/IStoreSchema.d.ts
+++ lib/interfaces/IStoreSchema.d.ts
@@ -3,7 +3,7 @@ import { IPath } from "./IPath";
 import { IScope } from "./IScope";
 export declare type ValueSelector<TModel, TValue> = (state: TModel) => TValue;
 export declare type IsInstruction<TModel> = <TValue>(selector: IPath<TModel, TValue>) => boolean;
-export declare type Transformator<TState, T> = (instruction: IInstruction<TState, any>, is: IsInstruction<TState>, state: T) => IterableIterator<IInstruction<TState, any>>;
+export declare type Transformator<TState, T> = (instruction: IInstruction<TState, any>, is: IsInstruction<TState>, state: T, storeState: TState) => IterableIterator<IInstruction<TState, any>>;
 export interface IStoreSchema<TState, TModel> {
     getState(state: TState): TModel;
     addScope(scope: IScope<TState, TModel, any>): any;
